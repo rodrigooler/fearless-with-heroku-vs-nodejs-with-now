9773513f89337b30fce26e3bf1347b224800c510
Oler <roodrigoprogrammer@gmail.com>
2019-04-30 22:06:23 -0300

    add listen in fearless

--- fearless-with-heroku/src/index.js
+++ fearless-with-heroku/src/index.js
@@ -1,14 +1,22 @@
 const { fearless, get, post, send, sendAsync } = require("@fearless/fearless");
 const json = require("@fearless/json");
 
-fearless([
-  get("/", (req, res) => {
-    send(res, 200, "Hello, World!");
-  }),
-  get("/", (req, res) => {
-    sendAsync(res, 200, async () => {
-      const data = await json(res);
-      return data;
-    });
-  })
-]);
+const options = {
+  handlers: [
+    get("/", (req, res) => {
+      send(res, 200, "Hello, World!");
+    }),
+    get("/", (req, res) => {
+      sendAsync(res, 200, async () => {
+        const data = await json(res);
+        return data;
+      });
+    })
+  ],
+  listen: {
+    port: process.env.PORT,
+    handler: () => {}
+  }
+};
+
+fearless(options);
